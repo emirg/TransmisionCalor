31df7b322bf01c4c0c143c7a9471dbb386488c58
Cristian Vincenzini <borgesvive@hotmail.com>
2018-06-24 16:42:40 -0300

    Create header.h
    Fichero cabecera para manipular la salida de resultados a ficheros

---
+++ MPI/header.h
@@ -0,0 +1,9 @@
+// Salida a archivo: comentar para que no salga
+#define conSalida
+
+// Tipo boolean
+typedef enum { false, true } bool;
+
+// Constantes
+const float Cx = 0.1;
+const float Cy = 0.1;
